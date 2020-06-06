d30a9a5d2a30d814fd1b4b14dcc3ca1fb194a699
apdr <anne.pedromisas@gmail.com>
2020-06-06 10:41:07 +0200

    Test sw

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
-window.onload = () => {
-  'use strict';
-
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-             .register('./sw.js');
-  }
-};
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js')
+  .then(function(registration) {
+    console.log('Registration successful, scope is:', registration.scope);
+  })
+  .catch(function(error) {
+    console.log('Service worker registration failed, error:', error);
+  });
+}
 
 $(document).ready(function() {
   $("[data-link]").click(function() {
