d3ed509836d5b2164b0bd21849e6c029d7e09138
apdr <anne.pedromisas@gmail.com>
2020-06-06 11:55:51 +0200

    test

--- sw.js
+++ sw.js
@@ -13,7 +13,6 @@ self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open(cacheName).then(function(cache) {
       console.log('[Service Worker] Caching all: app shell and content');
-      return cache.addAll();
     })
   );
 });
