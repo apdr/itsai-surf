6eb6e1fa3c87756141b2cd1b03cc92bb5b5790dd
apdr <anne.pedromisas@gmail.com>
2020-06-06 10:33:26 +0200

    Test

--- sw.js
+++ sw.js
@@ -1,42 +1,25 @@
-// CODELAB: Add list of files to cache here.
-const FILES_TO_CACHE = [
+var cacheName = 'itsaii-surf';
+var filesToCache = [
   '/',
   '/index.html',
   '/css/style.css',
   '/js/main.js'
 ];
 
-// CODELAB: Precache static resources here.
-evt.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log('[ServiceWorker] Pre-caching offline page');
-      return cache.addAll(FILES_TO_CACHE);
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
     })
-);
+  );
+});
 
-// CODELAB: Remove previous cached data from disk.
-evt.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(keyList.map((key) => {
-        if (key !== CACHE_NAME) {
-          console.log('[ServiceWorker] Removing old cache', key);
-          return caches.delete(key);
-        }
-      }));
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
     })
-);
-
-// CODELAB: Add fetch event handler here.
-if (evt.request.mode !== 'navigate') {
-  // Not a page navigation, bail.
-  return;
-}
-evt.respondWith(
-    fetch(evt.request)
-        .catch(() => {
-          return caches.open(CACHE_NAME)
-              .then((cache) => {
-                return cache.match('index.html');
-              });
-        })
-);
+  );
+});
