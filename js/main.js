3419c2146275baf0d991b6cecd17a4518e7c16d1
apdr <anne.pedromisas@gmail.com>
2020-06-06 10:45:23 +0200

    fix sw

--- js/main.js
+++ js/main.js
@@ -8,6 +8,15 @@ if ('serviceWorker' in navigator) {
   });
 }
 
+// Listen for install event, set callback
+self.addEventListener('install', function(event) {
+    // Perform some task
+});
+
+self.addEventListener('activate', function(event) {
+  // Perform some task
+});
+
 $(document).ready(function() {
   $("[data-link]").click(function() {
     window.location.href = $(this).attr("data-link");
